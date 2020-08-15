74a9f898f9095074d7de5cc6d705d7e6ef3a86de
unknown <tefip14@gmail.com>
2020-08-15 13:25:11 -0300

    commit

--- main.js
+++ main.js
@@ -10,3 +10,33 @@ document.querySelector('.menu-btn').addEventListener('click', () => {
 ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal('.news-cards', { delay: 800});
 ScrollReveal().reveal('.cards-brands', { delay: 800});
+
+//OCULTAR CONTAINER 1//
+
+let container = document.getElementsByClassName('container')
+
+
+// MOSTRAR CONTAINER2-SECCION NOSOTROS//
+
+let container2 = document.getElementsByClassName('container2')
+
+
+
+
+//MOSTRAR SECCION VARICES//
+
+let btnVarices = document.getElementById('btnVarices');
+let varicesContainer = document.getElementById('varicesContainer');
+
+btnVarices.addEventListener('click', showHide);
+
+function showHide (){ 
+
+    if(varicesContainer.style.display == "") {
+        varicesContainer.style.display = "flex";
+
+    }else if(varicesContainer.style.display == "flex"){
+        varicesContainer.style.display = "";
+    }
+  }
+
